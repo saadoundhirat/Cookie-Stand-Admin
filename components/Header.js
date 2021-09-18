c3c96778900c3ffbfc2e7e2077fd1fb485472314
saadoundhirat <saadoundhirat93@gmail.com>
2021-09-18 18:16:11 +0300

    feat: adding the form and style to the app

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+
+    return (
+        <header className="p-4 bg-green-500 text-black-50">
+            <h1 className="text-4xl">Cookie Stand Admin</h1>
+        </header>
+    )
+}
